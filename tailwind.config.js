7fb85dd04263f6c040295fb114b61c1ceba78577
vedanti-wss <vedantip@wharfstreetstudios.com>
2026-02-10 20:59:02 +0530

    index

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,12 +1,11 @@
-
 /** @type {import('tailwindcss').Config} */
 export default {
   content: [
     "./index.html",
-    "./**/*.{js,ts,jsx,tsx}",
+    "./**/*.{ts,tsx}"
   ],
   theme: {
     extend: {},
   },
   plugins: [],
-}
+};
